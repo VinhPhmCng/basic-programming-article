933ba9aa3738526a3eb7c2173fcae424e6880c89
VPC <111203113+VinhPhmCng@users.noreply.github.com>
2024-03-18 11:21:58 +0700

    Deploy

--- generated/trace/subsec-c-play-3.js
+++
@@ -1,4 +0,0 @@
-if (allTraceData === undefined) {
- var allTraceData = {};
- }
- allTraceData[""] = {"code": "# This is a Python cell\n# Write and run your Python code here\n\nprint(\"Hello world!\")\n\n", "trace": [{"exception_msg": "error: invalid preprocessing directive #This", "line": 1, "event": "uncaught_exception"}]}
