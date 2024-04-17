2fcb061468350d547828b1ad6171c4281dc70c20
VPC <111203113+VinhPhmCng@users.noreply.github.com>
2024-04-17 17:26:33 +0700

    Deploy
    Update alias

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,7 +7,7 @@ var ptx_lunr_docs = [
   "type": "Front Matter",
   "number": "",
   "title": "Front Matter",
-  "body": "   Phạm Công Vinh  github.com\/VinhPhmCng   vinhpcdev@gmail.com        copyright 2024 Phạm Công Vinh  Permission is granted to copy, distribute and\/or modify this document under the terms of the GNU Free Documentation License, Version 1.3 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts. A copy of the license is included in the section entitled GNU Free Documentation License .   "
+  "body": "   Devinh  github.com\/VinhPhmCng   vinhpcdev@gmail.com        copyright 2024 Phạm Công Vinh  Permission is granted to copy, distribute and\/or modify this document under the terms of the GNU Free Documentation License, Version 1.3 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts. A copy of the license is included in the section entitled GNU Free Documentation License .   "
 },
 {
   "id": "sec-introduction",
